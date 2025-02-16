abb76ba678347f2592f59abdda9dd403deacdf84
kimi.casamina <kimi.casamina@gmail.com>
2025-02-16 20:46:22 +0800

    fix jwt utils

--- backend/src/utils/jwtUtils.ts
+++ backend/src/utils/jwtUtils.ts
@@ -3,22 +3,27 @@ import { CustomError } from './customError';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const JWT_EXPIRATION = process.env.JWT_EXPIRATION || '1h';
-const JWT_SECRET = process.env.JWT_SECRET || 'secret';
-
 export const generateToken = (
   id: string,
   email: string,
   username: string,
 ): string => {
-  return jwt.sign({ id, email, username }, JWT_SECRET, {
-    expiresIn: JWT_EXPIRATION,
-  });
+  // return jwt.sign({ id, email, username }, JWT_SECRET, {
+  //   expiresIn: JWT_EXPIRATION,
+  // });
+
+  const secret = process.env.JWT_SECRET as string;
+  const options = { expiresIn: '1h' };
+
+  // Adding typings to the payload for better clarity
+  const payload = { id, email, username };
+  return jwt.sign(payload, secret, options);
 };
 
 export const verifyToken = (token: string) => {
+  const secret = process.env.JWT_SECRET as string;
   try {
-    return jwt.verify(token, JWT_SECRET);
+    return jwt.verify(token, secret);
   } catch (error) {
     throw new CustomError('Invalid or expired token', 401);
   }
